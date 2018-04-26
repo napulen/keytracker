1c5e81500b3871536519dad9122a7d538822ac0d
Nestor Napoles <napulen@gmail.com>
2018-04-26 16:58:17 -0400

    Adding more documentation about the matrix

--- key_transitions.py
+++ key_transitions.py
@@ -1,15 +1,17 @@
 """Key-transition probabilities
 
-This probabilities were computed from a very
+These probabilities were computed from a very
 simple model of bidimensional key distance:
 
-a 	C 	c 	Eb 	eb 	Gb 	gb
-e 	G 	g 	Bb 	bb 	Db 	db
-b 	D 	d 	F 	f 	Ab 	ab
-f# 	A 	a 	C 	c 	Eb 	eb
-c# 	E 	e 	G 	g 	Bb 	bb
-g# 	B 	b 	D 	d 	F 	f
-d# 	F# 	f# 	A 	a 	C 	c
+D	d 	F 	f 	Ab 	ab 	Cb 	cb 	Ebb 	
+A	a 	C 	c 	Eb 	eb 	Gb 	gb 	Bbb 	
+E	e 	G 	g 	Bb 	bb 	Db 	db 	Fb
+B	b 	D 	d 	F 	f 	Ab 	ab 	Cb
+F#	f# 	A 	a 	C 	c 	Eb 	eb 	Gb
+C#	c# 	E 	e 	G 	g 	Bb 	bb 	Db
+G#	g# 	B 	b 	D 	d 	F 	f 	Ab
+D#	d# 	F# 	f# 	A 	a 	C 	c 	Eb
+A#	a# 	C# 	c# 	E 	e 	G 	g 	Bb
 
 C is in the middle, the distance to every key
 is computed from the geometric distance in
@@ -22,15 +24,35 @@ i.e., it is further away from C:
 C
 G F a c
 d e f g
-Bb D A Eb
-bb Ab E b
-B Db
-f# eb
-ab c#
+D Eb A Bb
+E Ab bb b
+Db B
+eb f#
+c# ab
 F#
+
+a
+C A d e
+D E F G
+c f# g b
+Bb B c# f
+g# bb
+Eb F#
+C# Ab
+d#
+
+c
+C Eb f g
+F G Ab Bb
+d eb bb a
+Db D e ab
+db b
+A Gb
+Cb E
+f#  
 """
 
-key_transitions = [
+key_transitions_major = [
 	9/132, 4/132, 6/132, 6/132, 5/132, 8/132,
 	1/132, 8/132, 5/132, 6/132, 6/132, 4/132,
 	8/132, 2/132, 7/132, 3/132, 7/132, 7/132,
