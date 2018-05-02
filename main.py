975e45a9ceee7ad5ee939f0f6226ab8faaa80bfe
Nestor Napoles <napulen@gmail.com>
2018-05-02 08:58:35 -0400

    Formatting: Removing (almost) all errors shown by SublimeLinter-pycodesyle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """keytracker
 
-Creates the parameters for a Hidden Markov Model that finds the key of a sequence of notes
+Creates the parameters for a Hidden Markov Model
+that finds the key of a sequence of notes
 
 Nestor Napoles (napulen@gmail.com)
 """
@@ -13,7 +14,7 @@ import music21
 import numpy as np
 
 states = (
-	'C', 'Db', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B',
+    'C', 'Db', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B',
     'c', 'c#', 'd', 'eb', 'e', 'f', 'f#', 'g', 'ab', 'a', 'bb', 'b',
 )
 
@@ -26,13 +27,14 @@ start_p = {
     'ab': 1.0/24.0, 'a': 1.0/24.0, 'bb': 1.0/24.0, 'b': 1.0/24.0,
 }
 
+
 def create_transition_probabilities(transitions="key_transitions_exponential"):
     """Returns the transition probabilities
 
     The 'distance' between two keys is determined
     by a matrix of the form:
-    D   d   F   f   Ab  ab  Cb  cb  Ebb     
-    A   a   C   c   Eb  eb  Gb  gb  Bbb     
+    D   d   F   f   Ab  ab  Cb  cb  Ebb
+    A   a   C   c   Eb  eb  Gb  gb  Bbb
     E   e   G   g   Bb  bb  Db  db  Fb
     B   b   D   d   F   f   Ab  ab  Cb
     F#  f#  A   a   C   c   Eb  eb  Gb
@@ -46,8 +48,8 @@ def create_transition_probabilities(transitions="key_transitions_exponential"):
     do that and transpose it to all keys
     """
     d = dict()
-    key_transitions = getattr(kt, transitions)    
-    for idx,key in enumerate(states):
+    key_transitions = getattr(kt, transitions)
+    for idx, key in enumerate(states):
         pat1 = key_transitions[:12]
         pat2 = key_transitions[12:]
         shift1 = idx % 12
@@ -55,21 +57,22 @@ def create_transition_probabilities(transitions="key_transitions_exponential"):
             shift1 = idx
             shift2 = idx
         else:   # minor keys
-            pat1, pat2 = pat2, pat1            
+            pat1, pat2 = pat2, pat1
             shift1 = (idx + 3) % 12
             shift2 = idx % 12
         probs1 = collections.deque(pat1)
         probs2 = collections.deque(pat2)
         probs1.rotate(shift1)
         probs2.rotate(shift2)
-        kt_ = np.array(list(probs1) + list(probs2), dtype='float64')         
-        d[key] = {key:kt_[idx] for idx,key in enumerate(states)}
+        kt_ = np.array(list(probs1) + list(probs2), dtype='float64')
+        d[key] = {key: kt_[idx] for idx, key in enumerate(states)}
     return d
+
 
 def create_emission_probabilities(profiles="krumhansl_kessler"):
     """Returns the emission probabilities"""
     d = dict()
-    for idx,key in enumerate(states):
+    for idx, key in enumerate(states):
         if idx < 12:    # major keys
             profile = profiles + "_major"
         else:
@@ -78,34 +81,39 @@ def create_emission_probabilities(profiles="krumhansl_kessler"):
         key_profiles = collections.deque(key_profiles)
         key_profiles.rotate(idx % 12)
         key_profiles = np.array(list(key_profiles), dtype='float64')
-        d[key] = {pc:key_profiles[pc] for pc in range(12)}
+        d[key] = {pc: key_profiles[pc] for pc in range(12)}
     return d
 
+
 def create_observation_list(midi_file):
-	s = music21.converter.parse(midi_file)
-	obs = list()
-	for note in s.flat.notes:
-		if type(note) is music21.chord.Chord:
-			obs.extend(note.orderedPitchClasses)
-		else:
-			obs.append(note.pitch.pitchClass)
-	return obs
+    s = music21.converter.parse(midi_file)
+    obs = list()
+    for note in s.flat.notes:
+        if type(note) is music21.chord.Chord:
+            obs.extend(note.orderedPitchClasses)
+        else:
+            obs.append(note.pitch.pitchClass)
+    return obs
+
 
 def viterbi(obs, states, start_p, trans_p, emit_p):
     V = [{}]
     for st in states:
-        V[0][st] = {"prob": np.log(start_p[st]) + np.log(emit_p[st][obs[0]]), "prev": None}
+        V[0][st] = {
+            "prob": np.log(start_p[st])
+            + np.log(emit_p[st][obs[0]]), "prev": None
+            }
     # Run Viterbi when t > 0
     for t in range(1, len(obs)):
         V.append({})
-        for st in states:            
+        for st in states:
             max_tr_prob = max(V[t-1][prev_st]["prob"] + np.log(trans_p[prev_st][st]) for prev_st in states)
             for prev_st in states:
                 if V[t-1][prev_st]["prob"] + np.log(trans_p[prev_st][st]) == max_tr_prob:
                     max_prob = max_tr_prob + np.log(emit_p[st][obs[t]])
                     V[t][st] = {"prob": max_prob, "prev": prev_st}
                     break
-    #for line in dptable(V):
+    # for line in dptable(V):
     #    print(line)
     opt = []
     # The highest probability
@@ -122,37 +130,35 @@ def viterbi(obs, states, start_p, trans_p, emit_p):
         opt.insert(0, V[t + 1][previous]["prev"])
         previous = V[t + 1][previous]["prev"]
 
-    print('The steps of states are ' + ' '.join(opt) + ' with highest probability of %s' % max_prob)
+    print('The steps of states are '
+          + ' '.join(opt)
+          + ' with highest probability of %s' % max_prob)
     return opt, max_prob
 
-def dptable(V):
-    # Print a table of steps from dictionary
-    yield " ".join(("%12d" % i) for i in range(len(V)))
-    for state in V[0]:
-        yield "%.7s: " % state + " ".join("%.7s" % ("%f" % v[state]["prob"]) for v in V)
 
-
-trans_p = create_transition_probabilities(transitions='key_transitions_exponential_10')
+transitions = 'key_transitions_exponential_10'
+trans_p = create_transition_probabilities(transitions=transitions)
 emit_p = create_emission_probabilities(profiles='sapp')
 
 obs = create_observation_list('midi/ballade_g.mid')
 
-#obs = [0, 1, 4, 5, 7, 8, 10, 0]
+# obs = [0, 1, 4, 5, 7, 8, 10, 0]
 
-#print(obs)
-#pp.pprint(trans_p)
+# print(obs)
+# pp.pprint(trans_p)
 
 state_list, max_prob = viterbi(obs, states, start_p, trans_p, emit_p)
 
-#pp.pprint(trans_p)
+# pp.pprint(trans_p)
 
-obs = state_list # the keys become the observations
-states # remain the same
-emit_p = trans_p # the transition probabilities become the observation probabilities
-trans_p = create_transition_probabilities(transitions='key_transitions_null') # no modulations
-start_p #remains the same
+obs = state_list  # the keys become the observations
+# states, remains the same
+emit_p = trans_p  # the transition probs become the observation probs
+# No modulations
+transitions = 'key_transitions_null'
+trans_p = create_transition_probabilities(transitions=transitions)
+# start_p, remains the same
 
-#pp.pprint(trans_p)
+# pp.pprint(trans_p)
 
 key, max_prob = viterbi(obs, states, start_p, trans_p, emit_p)
-
